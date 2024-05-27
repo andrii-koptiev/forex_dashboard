a2af8117db1771f5d952d3dbf664867272074ade
Andrii Koptiev <koptev.andrey87@gmail.com>
2024-05-27 10:18:58 -0400

    fixed users page search params

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,10 +1,18 @@
 import { resetDatabase } from 'app/actions/resetDatabase';
 import Button from 'app/ui/Button';
 import LeaderBoard from 'app/ui/LeaderBoard';
+import { isEmpty } from 'lodash';
+import { redirect } from 'next/navigation';
 import { RouteParams } from 'types';
-import { RESET_DATABASE_BUTTON } from 'utils';
+import { RESET_DATABASE_BUTTON, getInitialRedirectUrl } from 'utils';
 
-const UsersPage = ({ searchParams }: RouteParams) => {
+const UsersPage = async ({ searchParams }: RouteParams) => {
+  if (isEmpty(searchParams)) {
+    const url = await getInitialRedirectUrl();
+
+    redirect(url);
+  }
+
   return (
     <div className='flex flex-col gap-9'>
       <div className='flex justify-end'>
