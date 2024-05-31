dcfc2c23446cc0389304611f678b128f5d7b961a
Andrii Koptiev <koptev.andrey87@gmail.com>
2024-05-30 22:12:09 -0400

    vercel json added

--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,10 @@
+import cors from 'cors';
 import express, { Express } from 'express';
 
 const app: Express = express();
 const PORT = 8080;
 
-// app.use(cors());
+app.use(cors());
 
 app.get('/api/home', (req, res) => {
   res.json({
